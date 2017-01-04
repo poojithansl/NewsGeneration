a936784257ddce76b21a832c4109d772c99023ed
sa7i <poojithansl007@gmail.com>
2017-01-04 10:37:09 +0530

    Model updated

--- LMusingRNNS/model.py
+++ LMusingRNNS/model.py
@@ -3,4 +3,16 @@ import tensorflow as tf
 
 from utils import InputText
 class Model():
-  
+    def __init__(self):
+        cell=tf.nn.rnn_cell.BasicLSTMCell
+        self.cell=tf.rnn_cell.MultiRNNCell([])
+        self.input_data = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
+        self.targets = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
+        self._initial_state = cell.zero_state(batch_size, data_type())
+
+
+
+
+
+def train():
+    data=InputText()
